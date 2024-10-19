530fdd08cf8ab6fd30b4b9b76fb03773189c9d55
Владимир Казаков <ilyawersed@mail.ru>
2024-10-19 21:58:26 +0300

    Implemented a few functions

--- src/regs.rs
+++ src/regs.rs
@@ -28,4 +28,32 @@ impl Regs {
             r_progcount: 0x0,
         }
     }
+
+    pub fn get_by_num(&self, addr: u8) -> u16 {
+        match addr {
+            0x0 => self.r0,
+            0x1 => self.r1,
+            0x2 => self.r2,
+            0x3 => self.r3,
+            0x4 => self.r4,
+            0x5 => self.r5,
+            0x6 => self.r6,
+            0x7 => self.r7,
+            _ => panic!("Incorrect register is being read!")
+        }
+    }
+
+    pub fn set_by_num(&mut self, addr: u8, value: u16) {
+        match addr {
+            0x0 => self.r0 = value,
+            0x1 => self.r1 = value,
+            0x2 => self.r2 = value,
+            0x3 => self.r3 = value,
+            0x4 => self.r4 = value,
+            0x5 => self.r5 = value,
+            0x6 => self.r6 = value,
+            0x7 => self.r7 = value,
+            _ => panic!("Incorrect register is being read!")
+        }
+    }
 }
--- src/vm.rs
+++ src/vm.rs
@@ -1,5 +1,8 @@
-pub use super::mem::{Mem, MEM_SIZE};
-pub use super::regs::{Regs, REG_SIZE};
+use core::panic;
+use std::u16;
+
+pub use super::mem::*;
+pub use super::regs::*;
 
 pub const PC_START: u16 = 0x3000;
 pub struct VM {
@@ -57,7 +60,7 @@ impl Command {
                 0xD => Opcode::OpRES,
                 0xE => Opcode::OpLEA,
                 0xF => Opcode::OpTRAP,
-                _ => Opcode::OpRES,
+                _ => panic!("Incorrect Opcode inside of the command constructor"),
             },
             value: val,
         }
@@ -69,6 +72,66 @@ impl VM {
         VM {
             memory: Mem::new(),
             regs: Regs::new(),
+        }
+    }
+
+    fn sign_extend(&self, value: u16, bit_count: u16) -> u16 {
+        if (value >> (bit_count - 1)) & 1 != 0 {
+            value | (0xFFFF << bit_count)
+        } else {
+            value
+        }
+    }
+
+    pub fn update_flags(&mut self, reg_num: u8) {
+        let reg_val = self.regs.get_by_num(reg_num);
+        if reg_val == 0 {
+            self.regs.r_cond = Flags::FlZro as u16;
+        } else if (reg_val >> 15) == 1 {
+            self.regs.r_cond = Flags::FlNeg as u16;
+        } else {
+            self.regs.r_cond = Flags::FlPos as u16;
+        }
+    }
+
+    pub fn op_add(&mut self, instr: u16) {
+        /* Destination Register (DR) */
+        let r0: u8 = ((instr >> 9) & 0x7) as u8;
+        /* First operand (SR1) */
+        let r1: u8 = ((instr >> 7) & 0x7) as u8;
+
+        if (instr >> 5) & 0x1 == 1 {
+            self.regs.set_by_num(
+                r0,
+                self.regs.get_by_num(r1) + self.sign_extend(instr & 0x1F, 5),
+            );
+        } else {
+            self.regs
+                .set_by_num(r0, self.regs.get_by_num(r1) + (instr & 0x7));
+        }
+
+        self.update_flags(r0);
+    }
+
+    pub fn op_ldi(&mut self, instr: u16) {
+        /* destination register (DR) */
+        let r0: u16 = (instr >> 9) & 0x7;
+        /* PCOffset9 */
+        let pc_offset: u16 = self.sign_extend(instr & 0x1F, 9);
+
+        let ptr: u16 = self
+            .memory
+            .get_mem((self.regs.r_progcount + pc_offset) as usize);
+
+        self.regs
+            .set_by_num(r0 as u8, self.memory.get_mem(ptr as usize));
+    }
+
+    pub fn execute(&mut self, cmd: Command) {
+        match cmd.opcode {
+            Opcode::OpADD => self.op_add(cmd.value),
+            Opcode::OpLDI => self.op_ldi(cmd.value),
+            _ => panic!("Usage of reserved Opcodes!"),
         }
     }
 }
