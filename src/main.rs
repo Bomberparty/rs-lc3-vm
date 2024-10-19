f699eccb87f7079c8e68fd71bdd256dd16a5a96d
Владимир Казаков <ilyawersed@mail.ru>
2024-10-20 01:06:44 +0300

    Add an ability to set image path to a file via args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use rs_lc_3_vm::vm::VM;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the binary image file
+    #[clap(short, long)]
+    image: String,
+}
 
 fn main() {
+    let args = Args::parse();
     let mut vm = VM::new();
-    vm.load_image("path/to/your/binary/image.obj").unwrap();
-    vm.run();
+
+    match vm.load_image(&args.image) {
+        Ok(_) => {
+            println!("Loaded image from: {}", args.image);
+            vm.run();
+        }
+        Err(e) => {
+            eprintln!("Failed to load image: {}", e);
+        }
+    }
 }
