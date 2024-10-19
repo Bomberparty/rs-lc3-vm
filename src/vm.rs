4f0727d2d751d58f98121cb2f0e4a3bfacb5875c
Владимир Казаков <ilyawersed@mail.ru>
2024-10-19 20:19:43 +0300

    Add VM RAII

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,16 @@
+use super::mem::{Mem, MEM_SIZE};
+use super::regs::{Regs, REG_SIZE};
+
 pub struct VM {
-    memory: todo!(),
-    regs: todo!(),
+    memory: Mem,
+    regs: Regs,
+}
+
+impl VM {
+    pub fn new() -> Self {
+        VM {
+            memory: Mem::new(),
+            regs: Regs::new(),
+        }
+    }
 }
