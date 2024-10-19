c74f48fca4de6794afec2bac57befab16449e01a
Владимир Казаков <ilyawersed@mail.ru>
2024-10-19 20:28:48 +0300

    Add Opcodes

--- src/vm.rs
+++ src/vm.rs
@@ -6,6 +6,25 @@ pub struct VM {
     regs: Regs,
 }
 
+pub enum Opcode {
+    OpBR = 0x0,   /* branch */
+    OpADD = 0x1,  /* add  */
+    OpLD = 0x2,   /* load */
+    OpST = 0x3,   /* store */
+    OpJSR = 0x4,  /* jump register */
+    OpAND = 0x5,  /* bitwise and */
+    OpLDR = 0x6,  /* load register */
+    OpSTR = 0x7,  /* store register */
+    OpRTI = 0x8,  /* unused */
+    OpNOT = 0x9,  /* bitwise not */
+    OpLDI = 0xA,  /* load indirect */
+    OpSTI = 0xB,  /* store indirect */
+    OpJMP = 0xC,  /* jump */
+    OpRES = 0xD,  /* reserved (unused) */
+    OpLEA = 0xE,  /* load effective address */
+    OpTRAP = 0xF, /* execute trap */
+}
+
 impl VM {
     pub fn new() -> Self {
         VM {
