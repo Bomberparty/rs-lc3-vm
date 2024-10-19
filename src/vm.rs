b21a3162b93f11256cb868e1429d8282b9ad9fb4
Владимир Казаков <ilyawersed@mail.ru>
2024-10-19 21:00:31 +0300

    Change the visibility of the VM fields

--- src/vm.rs
+++ src/vm.rs
@@ -1,9 +1,10 @@
-use super::mem::{Mem, MEM_SIZE};
-use super::regs::{Regs, REG_SIZE};
+pub use super::mem::{Mem, MEM_SIZE};
+pub use super::regs::{Regs, REG_SIZE};
 
+pub const PC_START: u16 = 0x3000;
 pub struct VM {
-    memory: Mem,
-    regs: Regs,
+    pub memory: Mem,
+    pub regs: Regs,
 }
 
 pub enum Opcode {
@@ -23,6 +24,44 @@ pub enum Opcode {
     OpRES = 0xD,  /* reserved (unused) */
     OpLEA = 0xE,  /* load effective address */
     OpTRAP = 0xF, /* execute trap */
+}
+
+pub enum Flags {
+    FlPos = 0x1,
+    FlZro = 0x2,
+    FlNeg = 0x4,
+}
+
+pub struct Command {
+    opcode: Opcode,
+    value: u16,
+}
+
+impl Command {
+    pub fn new(val: u16) -> Self {
+        Command {
+            opcode: match val >> 12 {
+                0x0 => Opcode::OpBR,
+                0x1 => Opcode::OpADD,
+                0x2 => Opcode::OpLD,
+                0x3 => Opcode::OpST,
+                0x4 => Opcode::OpJSR,
+                0x5 => Opcode::OpAND,
+                0x6 => Opcode::OpLDR,
+                0x7 => Opcode::OpSTR,
+                0x8 => Opcode::OpRTI,
+                0x9 => Opcode::OpNOT,
+                0xA => Opcode::OpLDI,
+                0xB => Opcode::OpSTI,
+                0xC => Opcode::OpJMP,
+                0xD => Opcode::OpRES,
+                0xE => Opcode::OpLEA,
+                0xF => Opcode::OpTRAP,
+                _ => Opcode::OpRES,
+            },
+            value: val,
+        }
+    }
 }
 
 impl VM {
