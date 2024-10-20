94826da3472302157e76c12568fe753f63741d9c
Владимир Казаков <ilyawersed@mail.ru>
2024-10-20 18:56:41 +0300

    Add flag register and fix issues with loading address

--- src/vm.rs
+++ src/vm.rs
@@ -3,6 +3,7 @@ use super::command::Command;
 
 pub struct VM {
     registers: [u16; 8],
+    flags: u16, // 3 flags: FlPos (0x0), FlZro (0x1), FlNeg (0x2)
     memory: [u16; 65536],
 }
 
@@ -10,6 +11,7 @@ impl VM {
     pub fn new() -> Self {
         VM {
             registers: [0; 8],
+            flags: 0,
             memory: [0; 65536],
         }
     }
@@ -19,25 +21,25 @@ impl VM {
             return Err(io::Error::new(io::ErrorKind::InvalidData, "File too short"));
         }
 
-        let mut start_address = ((buffer[0] as u16) << 8) | buffer[1] as u16;
+        let start_address = self.extract_start_address(buffer);
         let mut index = 2;
 
         while index + 1 < buffer.len() {
-            if start_address as usize >= self.memory.len() {
+            if start_address as usize + (index - 2) / 2 >= self.memory.len() {
                 return Err(io::Error::new(io::ErrorKind::InvalidData, "Start address out of bounds"));
             }
 
             let instruction = ((buffer[index] as u16) << 8) | buffer[index + 1] as u16;
-            self.memory[start_address as usize] = instruction;
-            start_address += 1;
+            self.memory[(start_address as usize + (index - 2) / 2)] = instruction;
             index += 2;
         }
 
         Ok(())
     }
 
-    pub fn run(&mut self) {
-        let mut pc = 0;
+    pub fn run(&mut self, buffer: &[u8]) {
+        let start_address = self.extract_start_address(buffer);
+        let mut pc = start_address; // Set the program counter to the starting address
 
         loop {
             if pc as usize >= self.memory.len() {
@@ -54,6 +56,20 @@ impl VM {
             }
 
             // TODO: Implement the actual VM logic here
+        }
+    }
+
+    fn extract_start_address(&self, buffer: &[u8]) -> u16 {
+        ((buffer[0] as u16) << 8) | buffer[1] as u16
+    }
+
+    fn update_flags(&mut self, value: u16) {
+        if value == 0 {
+            self.flags = 0x1; // FlZro
+        } else if value & 0x8000 != 0 {
+            self.flags = 0x2; // FlNeg
+        } else {
+            self.flags = 0x0; // FlPos
         }
     }
 }
